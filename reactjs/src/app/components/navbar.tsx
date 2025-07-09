a2a3607df6cdc7a1950c7dd704d88e0ee8ac1b47
Rahul Pawar <rahulpwr377@gmail.com>
2025-07-09 20:43:11 +0530

    feat:project init

---
+++ reactjs/src/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return(
+        <>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#aboutus">About us</a></li>
+            </ul>
+        </>
+    )
+}
---
+++ reactjs/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbar from "./components/navbar";
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      <h1>Rahul</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9APxkj0xClmrU3PpMZglHQkx446nQPG6lA&s"/>
+    </>
+  );
+}
